915f83c37807de52544c2dea7574b82c50521b8c
Weiller Carvalho <99501431+weillercarvalho@users.noreply.github.com>
2024-11-14 02:29:44 -0300

    Update two-sum.js

--- 1-two-sum/two-sum.js
+++ 1-two-sum/two-sum.js
@@ -4,20 +4,17 @@
  * @return {number[]}
  */
 var twoSum = function(nums, target) {
-       let numToIndex = new Map();  // Mapa para armazenar os números e seus índices
+       let numToIndex = new Map(); 
 
     for (let i = 0; i < nums.length; i++) {
-        let complement = target - nums[i];  // Calcula o complemento do número atual
+        let complement = target - nums[i];  
 
-        // Verifica se o complemento já existe no mapa
+       
         if (numToIndex.has(complement)) {
-            // Se o complemento está no mapa, retorna os índices do complemento e do número atual
             return [numToIndex.get(complement), i];  
         }
-
-        // Se o complemento não está no mapa, adiciona o número atual e seu índice ao mapa
         numToIndex.set(nums[i], i);
     }
 
-    return [];  // Retorna um array vazio se nenhum par for encontrado
+    return []; 
 };
