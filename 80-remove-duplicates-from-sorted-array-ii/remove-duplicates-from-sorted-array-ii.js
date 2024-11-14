8785595b53bb3a82db0e83582caf7bd8b9d1e13d
Weiller Carvalho <99501431+weillercarvalho@users.noreply.github.com>
2024-11-14 02:21:05 -0300

    Time: 66 ms (37.07%) | Memory: 51.8 MB (68.57%) - LeetSync

---
+++ 80-remove-duplicates-from-sorted-array-ii/remove-duplicates-from-sorted-array-ii.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var removeDuplicates = function(nums) {
+    if (nums.length <= 2) return nums.length;
+    let k = 2;
+    for (let i = 2; i < nums.length; i++) {
+        if (nums[i] !== nums[k-2]) {
+            nums[k++] = nums[i];
+        }
+    }
+    return k;
+};
